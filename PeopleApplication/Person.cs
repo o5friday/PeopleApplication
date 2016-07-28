c47505e0e02251aa86981c53a9738d2d6598965c
o5friday <darya.yemzhina@gmail.com>
2016-07-29 00:22:43 +0300

    initial

---
+++ PeopleApplication/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace PeopleApplication
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person() { }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() + LastName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person)
+            {
+                Person p = (Person)obj;
+                return p.FirstName.Equals(this.FirstName) && p.LastName.Equals(this.LastName);
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ PeopleApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeopleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person("Alex", "Ivanov");
+            var p2 = new Person("Alex", "Ivanov");
+            var p3 = new Person("Alex", "Green");
+
+            Console.WriteLine(p1.Equals(p2)); // true
+            Console.WriteLine(p1.Equals(p3)); // false
+
+            var people = new List<Person>()
+            {
+                new Person("Henry", "Qwerty"),
+                new Person("Alex", "Ivanov"),
+                new Person("Andrew", "Miles")
+            };
+
+            foreach(var person in people.Where(p => p.Equals(p1)))
+            {
+                Console.WriteLine(person.ToString()); // Alex Ivanov
+            }
+        }
+    }
+}
